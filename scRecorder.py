68e19d189e3059a8ade2aec278d132596a3bc014
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2022-11-19 10:33:15 +0800

    Coded Display Window Config

---
+++ scRecorder.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import pyscreenrec as pys
+
+mainBox = Tk()
+
+mainBox.geometry("700x150")
+mainBox.config(background="#282C32")
+mainBox.title("Screen Recorder")
+mainBox.resizable(False, False)
+
+mainBox.mainloop()
+
